6a0e2bf003a4280f2b763095b4ebfe04484d980b
mayconmrr <maycon.mrr@gmail.com>
2020-03-07 11:37:47 -0700

    Add routes and index js for home and header

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import React from 'react';
 import "./App.scss";
 import { Container, Section } from 'rbx';
 import Header from "./components/header";
+import Routes from './routes';
 
 const App = () => (
   <div>
     <Header />
     <Section>
       <Container>
-        One Bit Food
+        <Routes />
       </Container>
     </Section>
   </div>
