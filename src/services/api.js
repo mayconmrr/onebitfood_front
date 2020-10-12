0f04395dbfb659cfc97c8fc597f5cf939b3c2be4
mayconmrr <maycon.mrr@gmail.com>
2020-10-12 16:07:25 -0700

    Try different approach to access env var 3

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,6 @@
 import axios from "axios";
 
-let baseUrl = process.env.REACT_APP_URL
-// "http://localhost:3001"
+let baseUrl = "http://localhost:3001"
 
 if (process.env.NODE_ENV === 'production') {
   baseUrl = process.env.REACT_APP_URL;
